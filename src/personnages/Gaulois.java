0bd252f56b341ecedde823f787438db75cbff0ac
rattazi hugo <hugo421674@gmail.com>
2023-02-14 09:26:29 +0100

    LesGaulois TP1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -24,10 +24,7 @@ public class Gaulois {
 
 	}
 
-	public void frapper(Romain romain) {
-		System.out.println(nom = " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
-	}
+
 
 //	@Override
 //	public String toString() {
@@ -36,6 +33,6 @@ public class Gaulois {
 
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
-		System.out.println(asterix);
+		System.out.println(asterix.getNom());
 	}
 }
